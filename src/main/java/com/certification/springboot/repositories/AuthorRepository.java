6c6a42a7ac25341313becc69ee77775f363c4719
Mario Lancheros <lancherosm@gmail.com>
2018-11-22 21:43:19 -0500

    Commit 2 : Spring DATA

--- src/main/java/com/certification/springboot/repositories/AuthorRepository.java
+++ src/main/java/com/certification/springboot/repositories/AuthorRepository.java
@@ -3,5 +3,10 @@ package com.certification.springboot.repositories;
 import com.certification.springboot.model.Author;
 import org.springframework.data.repository.CrudRepository;
 
+
+/**
+ * Spring Data contains a bunch of repositories that have many implemented methods for saving, deleting, updating data
+ * as CrudRepository in this example
+ */
 public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
